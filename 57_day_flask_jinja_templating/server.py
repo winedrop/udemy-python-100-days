3106ede4259d5ed82d3acb105f0d6eccc74e7004
winedrop <lumiielumiie@gmail.com>
2022-08-29 18:22:02 -0700

    completes day 58 website and starts day 59

--- 57_day_flask_jinja_templating/server.py
+++ 57_day_flask_jinja_templating/server.py
@@ -32,7 +32,7 @@ def guess(name_):
     return render_template("guess.html", name=name_, gender=gender_, age=age_)
 
 @app.route("/blog")
-def blog():
+def get_blog():
     blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
     response = requests.get(blog_url)
     all_posts = response.json()
