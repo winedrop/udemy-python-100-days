fe28e5f37cef9b53eade02bfe9b986047db3fb03
winedrop <lumiielumiie@gmail.com>
2022-08-30 18:51:59 -0700

    removes hardcoded blogs and loads them from api

--- 59_upgraded_blog_flask/server.py
+++ 59_upgraded_blog_flask/server.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
+blog_endpoint = "https://api.npoint.io/c790b4d5cab58020d391"
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("index.html", posts=all_posts)
 
 @app.route('/about')
 def about_page():
@@ -13,6 +18,13 @@ def about_page():
 @app.route('/contact')
 def contact_page():
     return render_template("contact.html")
+
+# @app.route('/blogs')
+# def get_blog():
+#     blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+#     response = requests.get(blog_url)
+#     all_posts = response.json()
+#     return render_template("blog.html", posts=all_posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
